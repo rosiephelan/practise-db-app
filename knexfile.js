5cd774329cb2f990b970c982ad52036ae2a52154
Rosie Phelan <rosiephelan@hotmail.co.uk>
2020-05-11 21:45:12 +0100

    added production obj to knex file

--- knexfile.js
+++ knexfile.js
@@ -16,8 +16,21 @@ module.exports = {
     }
     
   },
-  production: {}
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_url,
+    pool: {
+      min: 2,
+      max: 10, 
+    },
+    migrations: {
+      tablename: 'knex_migrations',
+      directory: './migrations'
+    }
+  }
 };
 
 // pool enforeces foreign ket relation beetween our tables 
 // pool runs after table mounted
+
+// connection = what points to our postgres table - the variable on heroku
